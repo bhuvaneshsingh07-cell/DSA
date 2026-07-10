ab49768eb70aee589a8904f1b79ad1305ae30f60
bhuvaneshsingh07-cell <bhuvaneshsingh07@gmail.com>
2026-07-10 22:27:53 +0530

    Time: 0 ms (100%), Space: 12.4 MB (62.11%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -3,13 +3,16 @@ class Solution(object):
         self.accounts=accounts
         m=len(accounts)
         n=len(accounts[0])
-        arr=[0]*m
+        
+        wealth=0
         for i in range(0,m):
             sum=0
             for j in range(0,n):
                 sum+=accounts[i][j]
-                arr[i]=sum
-        return max(arr)
+                
+            if sum>wealth:
+                wealth=sum
+        return wealth
 
     
         
