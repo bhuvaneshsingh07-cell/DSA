9b98341a2e460f6cb7ce06d7dab5822bdc15ffde
bhuvaneshsingh07-cell <bhuvaneshsingh07@gmail.com>
2026-07-11 15:45:31 +0530

    Time: 14 ms (6.36%), Space: 12.4 MB (18.16%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -2,9 +2,7 @@ class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         self.candies=candies
         self.extraCandies=extraCandies
-        
         a=[True]*len(candies)
-
         for i in range(0,len(candies)):
             d=candies[i]+extraCandies
             for j in range(0,len(candies)):
