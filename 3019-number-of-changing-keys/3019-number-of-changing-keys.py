412e5b9c9e444fe4b40ea951294f4975ef5bf9e0
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-07-22 20:41:24 +0530

    Time: 1 ms (61.02%), Space: 12.4 MB (52.54%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.py
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         count=0
         for i in range(len(s)-1):
-            sum=0
+           
             if s[i]==s[i+1] or s[i]==s[i+1].upper() or s[i]==s[i+1].lower() :
-                sum+=1
+                pass
             elif s[i]!=s[i+1] or s[i]!=s[i+1].upper() or s[i]!=s[i+1].lower() :
                 count+=1
         return count
