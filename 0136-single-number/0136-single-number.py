f1ace40c712470fd793c98e888b453e7b62ad801
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-07-27 20:00:32 +0530

    Time: 6 ms (38.93%), Space: 13.9 MB (51.64%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        x=0
+        for i in range(len(nums)):
+
+        
+            x=x^nums[i]
+        return x
+        
+        
+        
