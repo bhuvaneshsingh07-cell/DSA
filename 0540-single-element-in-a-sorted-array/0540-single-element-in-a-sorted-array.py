44d7a5c90b715bdd3f8122c5fac9254076f27a14
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-08-07 22:14:20 +0530

    Time: 7 ms (13.9%), Space: 16.3 MB (98.1%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor=0
+        for i in nums:
+            xor=xor^i
+        return xor
+        
+        
