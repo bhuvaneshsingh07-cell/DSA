9325f0335e557a66b87a2c55796028715bd5150c
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-08-10 20:40:36 +0530

    Time: 2356 ms (8.97%), Space: 13.4 MB (16.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,14 +4,13 @@ class Solution(object):
         self.target=target
     
         a=len(nums)
-        b=[0]*2
-        for i in range(0,a):
-            for j in range(1,a):
-                if i==j:
-                    pass
+        b=[0,0]
+        for i in range(0,a-1):
+            for j in range(i+1,a):
+              
                
-                elif nums[i]+nums[j]==target:
-                    b[1]=j
+                if nums[i]+nums[j]==target:
                     b[0]=i
+                    b[1]=j
                    
         return b 
