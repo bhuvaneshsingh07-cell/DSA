a112be4ede3167cf3261eece7b1571cbff898714
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-08-10 20:42:00 +0530

    Time: 2178 ms (13.51%), Space: 13.2 MB (53.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,10 +7,7 @@ class Solution(object):
         b=[0,0]
         for i in range(0,a-1):
             for j in range(i+1,a):
-              
-               
                 if nums[i]+nums[j]==target:
                     b[0]=i
                     b[1]=j
-                   
-        return b 
+                    return b 
