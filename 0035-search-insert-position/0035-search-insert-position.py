8c626fd4945545575b2f597d1676da135475d18e
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-08-22 22:53:44 +0530

    Time: 0 ms (100%), Space: 12.9 MB (30.43%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if nums[0]>target:
+            return 0
+        if nums[len(nums)-1]<target:
+            return len(nums)
+
+        
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+        for i in range(len(nums)-1):   
+            if nums[i]<=target and nums[i+1]>target:
+                return i+1 
+        
+        
