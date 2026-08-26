1b184412cd0dd956d47c781b64321a3836afe033
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-08-26 22:30:28 +0530

    Time: 3 ms (80.16%), Space: 12.4 MB (37.25%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        fhalf=""
-        shalf=""
+        n=len(s)//2
+        fhalf= s[n:]
+       
+        shalf=s[:n]
         fvowel=0
         svowel=0
         vowel="aeiouAEIOU"
-        for i in range(0,len(s)/2):
-            fhalf+=s[i]
-        for i in range(len(s)/2,len(s)):
-            shalf+=s[i]
+        
         for i in fhalf:
             if i in vowel:
                 fvowel+=1
