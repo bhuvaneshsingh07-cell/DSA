5049bb35ce3465a9a8fd889210802f011fd9660b
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-09-24 22:29:31 +0530

    Time: 4 ms (50.54%), Space: 12.3 MB (56.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,5 +1,15 @@
 class Solution(object):
-    def mySqrt(self, x):
-       c=sqrt(x)
-       d=int(c)
-       return d
+    def mySqrt(self, n):
+        start=0
+        end=n
+        ans=0
+        while start<=end:
+            mid=(start+end)/2
+            if mid*mid==n:
+                return mid
+            elif mid*mid<n:
+                ans=mid
+                start=mid+1
+            else:
+                end=mid-1
+        return ans
