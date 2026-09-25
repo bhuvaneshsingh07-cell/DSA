742718130cf6748af0d52eee54290fbff39c467f
BHUVNESH PRATAP SINGH <bhuvaneshsingh07@gmail.com>
2026-09-25 23:09:46 +0530

    Time: 0 ms (100%), Space: 13 MB (68.96%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        i=0
+        j=len(nums)-1
+
+
+        while i<=j:
+            if nums[i]%2!=0:
+
+                if nums[j]%2==0:
+                    nums[i],nums[j]=nums[j],nums[i]
+                else:
+                    j-=1
+            else:
+                i+=1
+        return nums
